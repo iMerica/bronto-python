6b477d53a4a8d820e03839212370a0a78dab4b11
Joey Wilhelm <joey@scottsmarketplace.com>
2013-12-27 10:36:41 -0700

    * Brought the API endpoint into the code * Made the suds client a "private" variable * Fixed get_contacts()

--- bronto/client.py
+++ bronto/client.py
@@ -1,6 +1,8 @@
 from suds import WebFault
 from suds.client import Client as SudsClient
 
+API_ENDPOINT = 'https://api.bronto.com/v4?wsdl'
+
 
 class BrontoError(Exception):
     pass
@@ -11,23 +13,20 @@ class Client(object):
                              'source', 'customSource', 'listIds', 'fields',
                              'SMSKeywordIDs']
 
-    def __init__(self, endpoint, token, **kwargs):
-        if not endpoint or not isinstance(endpoint, basestring):
-            raise ValueError('Must supply an endpoint as a non empty string.')
-        if not token or not isinstance(endpoint, basestring):
+    def __init__(self, token, **kwargs):
+        if not token or not isinstance(token, basestring):
             raise ValueError('Must supply a token as a non empty string.')
 
-        self._endpoint = endpoint
         self._token = token
-        self.client = None
+        self._client = None
 
     def login(self):
-        self.client = SudsClient(self._endpoint)
+        self._client = SudsClient(API_ENDPOINT)
         try:
-            self.session_id = self.client.service.login(self._token)
-            session_header = self.client.factory.create('sessionHeader')
+            self.session_id = self._client.service.login(self._token)
+            session_header = self._client.factory.create('sessionHeader')
             session_header.sessionId = self.session_id
-            self.client.set_options(soapheaders=session_header)
+            self._client.set_options(soapheaders=session_header)
         except WebFault as e:
             raise BrontoError(e.message)
 
@@ -38,7 +37,7 @@ class Client(object):
                 raise ValueError('Each contact must be a dictionary of contact attributes')
             if not any([contact.get('email'), contact.get('mobileNumber')]):
                 raise ValueError('Must provide either an email or mobileNumber')
-            contact_obj = self.client.factory.create('contactObject')
+            contact_obj = self._client.factory.create('contactObject')
             # FIXME: Add special handling for listIds, fields and SMSKeywordIDs
             for field, value in contact:
                 if field not in self._valid_contact_fields:
@@ -46,7 +45,7 @@ class Client(object):
                 setattr(contact_obj, field, value)
             final_contacts.append(contact_obj)
         try:
-            response = self.client.service.addContacts(final_contacts)
+            response = self._client.service.addContacts(final_contacts)
             if response.errors:
                 err_str = ', '.join(['%s: %s' % (response.results[x].errorCode,
                                                  response.results[x].errorString)
@@ -60,20 +59,27 @@ class Client(object):
     def add_contact(self, contact):
         return self.add_contacts([contact, ])
 
-    def get_contact(self, email):
-        contact_email = self.client.factory.create('stringValue')
-        filter_operator = self.client.factory.create('filterOperator')
-        contact_email.operator = filter_operator.EqualTo
-        contact_email.value = email
-
-        contact_filter = self.client.factory.create('contactFilter')
-        contact_filter.email = contact_email
-        filter_type = self.client.factory.create('filterType')
-        contact_filter.type = filter_type.AND
+    def get_contacts(self, emails):
+        final_emails = []
+        filter_operator = self._client.factory.create('filterOperator')
+        fop = filter_operator.EqualTo
+        for email in emails:
+            contact_email = self._client.factory.create('stringValue')
+            contact_email.operator = fop
+            contact_email.value = email
+            final_emails.append(contact_email)
+        contact_filter = self._client.factory.create('contactFilter')
+        contact_filter.email = final_emails
+        if len(final_emails) > 1:
+            filter_type = self._client.factory.create('filterType')
+            contact_filter.type = filter_type.OR
 
         try:
-            response = self.client.service.readContacts(contact_filter,
+            response = self._client.service.readContacts(contact_filter,
                                                         pageNumber=1)
         except WebFault as e:
             raise BrontoError(e.message)
         return response
+
+    def get_contact(self, email):
+        return self.get_contacts([email, ])
