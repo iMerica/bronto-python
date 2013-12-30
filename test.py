2db9a5e36aaac092658a9c389dcf40233b6fa3c5
Joey Wilhelm <joey@scottsmarketplace.com>
2013-12-30 13:03:04 -0700

    Implemented delete_contact() and moved add/delete into setUp/tearDown

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from bronto import client
 
 
 class BrontoTest(unittest.TestCase):
+    contact_info = {'email': 'joey@scottsmarketplace.com'}
 
     @classmethod
     def setUpClass(cls):
@@ -13,18 +14,20 @@ class BrontoTest(unittest.TestCase):
         cls._client = client.Client(cls._token)
         cls._client.login()
 
-
-class BrontoContactTest(BrontoTest):
-    contact_info = {'email': 'joey@scottsmarketplace.com'}
-
-    def test_add_contact(self):
+    def setUp(self):
         contact = self._client.add_contact(self.contact_info)
         self.assertIs(contact.isError, False)
+
+    def tearDown(self):
+        response = self._client.delete_contact(self.contact_info['email'])
+        self.assertIs(response.isError, False)
+
+
+class BrontoContactTest(BrontoTest):
 
     def test_get_contact(self):
         contact = self._client.get_contact(self.contact_info['email'])
         self.assertEqual(contact.email, self.contact_info['email'])
-
 
 if __name__ == '__main__':
     unittest.main()
